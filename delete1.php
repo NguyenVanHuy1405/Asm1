c0ae98f5d9eb2a2b76441e9433eaade31ccef38c
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2021-06-19 10:12:21 +0700

    Update delete1.php

--- delete1.php
+++ delete1.php
@@ -13,7 +13,7 @@ if($data)
 {
  echo "<script>alert('Delete Successfully!')</script>";
 ?>
-<meta http-equiv="refresh" content="0; url=https://localhost/shop1.php" />
+<meta http-equiv="refresh" content="0; url=https://fptapptech.herokuapp.com/shop1.php">
 <?php
 }
 else
