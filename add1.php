33002af6132dad237d9cb07b18d563666582b455
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2021-06-21 07:46:44 +0700

    Update add1.php

--- add1.php
+++ add1.php
@@ -1,10 +1,12 @@
 <?php
-      			$host_heroku = "ec2-34-233-114-40.compute-1.amazonaws.com";
-			$db_heroku = "d63vsqeg1u1bhr";
-			$user_heroku = "vcljjiechthrbs";
-			$pw_heroku = "91e24a4f6c609d6e3bd3c33019a26933d378b76f8e8c58956c617060bdf2bc12";
+      			$host_heroku = "ec2-3-231-69-204.compute-1.amazonaws.com";
+			$db_heroku = "d5boqk3bekfndr";
+			$user_heroku = "zweujvxfknurip";
+			$pw_heroku = "a34706f696373b39c20d08e062a4bba2c33d0f9febe6eeaa26e77985c92a9e5e";
+			# Create connection to Heroku Postgres
 			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			$pg_heroku = pg_connect($conn_string);
+			# Connect to DATABASE
+			$pg_conn = pg_connect($conn_string);
             if (!$pg_heroku)
             {
             die('Error: Could not connect: ' . pg_last_error());
