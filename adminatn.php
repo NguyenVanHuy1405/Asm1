2f26d9a9ed3b4df1bd782b933281bf44697e972c
NguyenVanHuy1405 <84852270+NguyenVanHuy1405@users.noreply.github.com>
2021-06-22 10:58:30 +0700

    Update adminatn.php

--- adminatn.php
+++ adminatn.php
@@ -4,6 +4,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  	<a href="index.php" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-log-out"></span> Log out</a> 
  </ul>
+	<h1 color:'red'> Welcome to Admin ATN </h1>
 	 <style>
  body {
  background-image: url('admin.jpeg');
